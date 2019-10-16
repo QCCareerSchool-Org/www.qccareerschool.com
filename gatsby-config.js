0f54c4803146b7840f1c11288b724fb2bbaa6148
Dave Welsh <dave@qccareerschool.com>
2019-10-16 10:59:52 -0400

    Added initial npm packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `QC Career School`,
+    description: ``,
+    author: `QC Career School`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,15 +18,46 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `QC Career School`,
+        short_name: `QC Career`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/Q.svg`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Playfair Display`,
+            variants: [`400`, `500`],
+            // subsets: [`latin`],
+          },
+          {
+            family: `Open Sans`,
+            variants: [`300`, `500`, `700`]
+          },
+        ],
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-google-gtag`,
+    //   options: {
+    //     trackingIds: [
+    //       `UA-3632503-9`,
+    //       `AW-1071836607`,
+    //     ],
+    //     gtagConfig: {
+    //       site_speed_sample_rate: 100,
+    //     },
+    //   },
+    // },
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-tslint`,
+    `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
